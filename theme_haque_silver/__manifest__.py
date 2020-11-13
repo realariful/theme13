d7447aa06fc09c12e5aac1339135e8a5b179248f
Real Haque <real_ariful@hotmail.com>
2020-11-14 04:36:45 +0600

    Update COde for Snippet

--- theme_haque_silver/__manifest__.py
+++ theme_haque_silver/__manifest__.py
@@ -1,35 +1,23 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "theme_haque_silver",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
-
+    'name': "Haque Silver  Theme",
+    'summary': """Haque Silver  Theme""",
+    'description': """Haque Silver  Theme""",
     'author': "My Company",
     'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
+    'category': 'Theme/Silver',
     'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
+    'depends': ['website', 'website_theme_install', 'website_blog', 'sale'],
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        # 'views/views.xml',
+        # 'views/templates.xml',
+        'views/layout.xml',
+        'views/pages.xml',
+        'views/assets.xml',
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    # 'demo': [
+    #     'demo/demo.xml',
+    # ],
 }
